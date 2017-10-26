cba06f156103bc5b21c49837d8f4b9f94ca95f1b
andresluna11 <32135066+andresluna11@users.noreply.github.com>
2017-10-26 08:34:34 +0200

    Update about.js

--- app/locales/es/about.js
+++ app/locales/es/about.js
@@ -2,7 +2,7 @@
 
 export default {
   'copy': 'La app de música para unir todos los servicios de música en streaming.  Poner canciones ' +
-    'De Sporify, Last.fm, Soundcloud, YouTube, mp3s. Crea listas de reproducción ' +
+    'De Spotify, Last.fm, Soundcloud, YouTube, mp3s. Crea listas de reproducción ' +
     'a través de servicios como YouTube, Soundcloud, Spotify, Last.fm, y más. ' +
     'Crea y comparte listas de reproducción a través de los servicios. Sigue a los artistas y promociona ' +
     'canciones.',
